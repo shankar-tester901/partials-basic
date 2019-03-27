c12652b98de4b59d070b1e0bd89b4b35711c0992
shankartester <shankar.tester901@gmail.com>
2019-03-27 14:09:12 +0530

    added extra

--- server.js
+++ server.js
@@ -8,6 +8,71 @@ app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
+// users array
+var usersList = [
+    {
+        id: 1,
+        name: 'Mukesh Chapagain',
+        age: 99,
+        email: 'mukesh@example.com'
+    },
+    {
+        id: 2,
+        name: 'Brad Pitt',
+        age: 80,
+        email: 'brad@example.com'
+    },
+    {
+        id: 3,
+        name: 'Steve Smith',
+        age: 56,
+        email: 'steve@example.com'
+    },
+    {
+        id: 4,
+        name: 'Darren Sammy',
+        age: 48,
+        email: 'sammy@example.com'
+    }
+];
+ 
+// for users list page
+app.get('/users', function(request, response){    
+    /**
+     * render to views/users.ejs template file
+     * usersList is set to users variable
+     */ 
+    response.render('pages/users', {users: usersList});
+});
+ 
+// for individual user page
+app.get('/user/:id', function(request, response){    
+    /** 
+     * Get the individual user details using request param ID
+     * 
+     * We use array.filter() function for this purpose
+     * 
+     * filter() is a Javascript function that creates a new array with elements 
+     * that satisfies the conditions present in the callback function
+     */ 
+    var singleUser = usersList.filter(function(user){console.log(user.id); return user.id == request.params.id});
+    
+    /** 
+     * The filter creates a new array with single user element
+     * Hence, getting the value of the first and only element
+     */ 
+    var singleUser = singleUser[0];
+    
+    /**
+     * render to views/user.ejs template file
+     * name, age & email variables are passed to the template
+     */ 
+    response.render('pages/user', {
+        name: singleUser.name, 
+        age: singleUser.age,
+        email: singleUser.email
+    });
+});
 // index page
 app.get('/', function(req, res) {
     var drinks = [
